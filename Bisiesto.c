470e90aaa6baeba7f2a4fc7f88253a2198152c3f
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2024-10-03 02:09:46 -0600

    InsertBisiesto

---
+++ Bisiesto.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Declaración de variables
+    int anio;
+    // Entrada de datos
+    printf("Ingrese un año: ");
+    scanf("%d", &anio);
+    // Proceso
+    if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+        printf("%d Bisiesto\n", anio);
+    } else {
+        printf("%d No Bisiesto.\n", anio);
+    }
+
+    return 0;
+}
