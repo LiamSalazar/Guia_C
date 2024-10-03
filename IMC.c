f50a9e1ded0d4787eb5b24a2a4e307cd7ac4a4de
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2024-10-03 04:31:26 -0600

    ModifyIMC

--- IMC.c
+++ IMC.c
@@ -14,9 +14,9 @@ int main() {
     printf("Su tipo de peso es: \n");
     if (imc < 18.5) {
         printf("Bajo peso\n");
-    } else if (imc >= 18.5 && imc < 24.9) {
+    } else if (imc >= 18.5 && imc < 25) {
         printf("Peso normal\n");
-    } else if (imc >= 25.0 && imc < 29.9) {
+    } else if (imc >= 25.0 && imc < 30) {
         printf("Sobrepeso\n");
     } else {
         printf("Obesidad\n");
