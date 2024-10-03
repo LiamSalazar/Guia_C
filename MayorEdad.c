5fe82c986794f69090b68115cafd52eb53dcbcd3
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2024-10-03 00:57:45 -0600

    AgregarMayorEdad

---
+++ MayorEdad.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+# define PARAMETRO 18
+int main(){
+    // Declaracion de variables
+    int edad = 0;
+    // Entrada de datos
+    printf("Ingrese su edad: \n");
+    scanf("%d", edad);
+    // Procesos
+    if (edad >= PARAMETRO){
+        printf("MAYOR DE EDAD \n");
+    }
+    else{
+        printf("MENOR DE EDAD \n");
+    }
+    return 0;
+}
